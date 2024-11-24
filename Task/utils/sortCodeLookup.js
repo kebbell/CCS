286e734ed1632febc151616a0b17dd511384cbce
Sam <samuelkebbell@gmail.com>
2024-11-24 08:51:08 +0000

    Uppdates

--- Task/utils/sortCodeLookup.js
+++ Task/utils/sortCodeLookup.js
@@ -1,6 +1,7 @@
 exports.lookupSortCode = async (sortCode) => {
     console.log(`Validating sort code: ${sortCode}`);
-    // Mock external API call
-    return Promise.resolve(true);
+    // Simulate an external API response
+    const validSortCodes = ['12-34-56', '98-76-54'];
+    return validSortCodes.includes(sortCode);
   };
   
